dc407c21c7de367995d6ee642b5d46941d0a32cc
lizeyang1995 <424011527@qq.com>
2020-09-14 18:12:54 +0800

    [lizeyang] feat: complete a test, should throw exception when calculation given number is negative

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,10 @@
 public class BowlingGame {
 
     public void calculate(int[] numberOfKnockdowns) {
-
+        for(int number : numberOfKnockdowns) {
+            if (number < 0) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
