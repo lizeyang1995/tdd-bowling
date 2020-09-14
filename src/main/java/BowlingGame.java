e993f09d0828958c9de7c818e06abb513d8d496d
lizeyang1995 <424011527@qq.com>
2020-09-14 18:18:40 +0800

    [lizeyang] feat: should throw exception when calculation given numbers less than 12

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,9 @@
 public class BowlingGame {
 
     public void calculate(int[] numberOfKnockdowns) {
+        if (numberOfKnockdowns.length < 12) {
+            throw new IllegalArgumentException();
+        }
         for(int number : numberOfKnockdowns) {
             if (number < 0) {
                 throw new IllegalArgumentException();
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
@@ -6,6 +7,13 @@ public class BowlingGameTest {
     void should_throw_exception_when_calculation_given_number_is_negative() {
         BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
+    }
+
+    @Test
+    void should_throw_exception_when_calculation_given_numbers_less_than_12() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int[] numberOfKnockdowns = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 }
