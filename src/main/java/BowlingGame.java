2be54a397506abd5aedf4c351f9ae23f6e4daf49
lizeyang1995 <424011527@qq.com>
2020-09-14 21:12:11 +0800

    [lizeyang] refactor: add a method named getSumScores

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -6,31 +6,35 @@ public class BowlingGame {
     public int calculate(int[] numberOfKnockdowns) {
         throwError(numberOfKnockdowns);
         for(int index = 0; index < numberOfKnockdowns.length; index++) {
-            int value = numberOfKnockdowns[index];
-            if (isFirstThrow && value != 10) {
-                isFirstThrow = false;
-                sumScores += value;
-                frameScore += value;
-                continue;
-            } else if (value == 10){
-                strikeFrame(numberOfKnockdowns, index);
-            } else if (frameScore + value == 10){
-                spareFrame(numberOfKnockdowns, index);
-            } else {
-                sumScores += value;
-            }
-            isFirstThrow = true;
-            frameScore = 0;
+            getSumScores(numberOfKnockdowns, index);
         }
         return sumScores;
     }
 
-    private void spareFrame(int[] numberOfKnockdowns, int index) {
+    private void getSumScores(int[] numberOfKnockdowns, int index) {
+        int value = numberOfKnockdowns[index];
+        if (isFirstThrow && value != 10) {
+            isFirstThrow = false;
+            sumScores += value;
+            frameScore += value;
+            return;
+        } else if (value == 10){
+            calculateStrikeFrame(numberOfKnockdowns, index);
+        } else if (frameScore + value == 10){
+            calculateSpareFrame(numberOfKnockdowns, index);
+        } else {
+            sumScores += value;
+        }
+        isFirstThrow = true;
+        frameScore = 0;
+    }
+
+    private void calculateSpareFrame(int[] numberOfKnockdowns, int index) {
         sumScores += numberOfKnockdowns[index];
         sumScores += numberOfKnockdowns[index + 1];
     }
 
-    private void strikeFrame(int[] numberOfKnockdowns, int index) {
+    private void calculateStrikeFrame(int[] numberOfKnockdowns, int index) {
         sumScores += numberOfKnockdowns[index];
         sumScores += numberOfKnockdowns[index + 1];
         sumScores += numberOfKnockdowns[index + 2];
