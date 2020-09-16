c8afc02666c8a2af483f573a0af8c3f9cdd7672e
lizeyang1995 <424011527@qq.com>
2020-09-16 10:11:04 +0800

    [lizeyang] refactor: Initialize bowlingGame

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,46 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
+
+    BowlingGame bowlingGame;
+
+    @BeforeEach
+    void initBowlingGame() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     void should_throw_exception_when_calculation_given_number_is_negative() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 
     @Test
     void should_throw_exception_when_calculation_given_number_is_larger_than_10() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 
     @Test
     void should_throw_exception_when_calculation_given_array_less_than_11() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 10, 10, 10, 10, 10, 10, 10, 10, 1};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 
     @Test
     void should_throw_exception_when_calculation_given_array_larger_than_21() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 5};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 
     @Test
     void should_throw_exception_when_two_throw_numbers_larger_than_10() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {0, 0, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         assertThrows(IllegalArgumentException.class, () -> bowlingGame.calculate(numberOfKnockdowns));
     }
 
     @Test
     void should_return_sum_when_every_frame_less_than_10() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(30, result);
@@ -48,7 +51,6 @@ public class BowlingGameTest {
 
     @Test
     void should_return_sum_when_calculate_given_array_exists_strike() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 1, 2, 10, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(50, result);
@@ -56,7 +58,6 @@ public class BowlingGameTest {
 
     @Test
     void should_return_sum_when_calculate_given_array_exists_spare() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 1, 2, 10, 5, 5, 1, 2, 1, 2, 5, 5, 1, 2, 1, 2, 1, 2};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(73, result);
@@ -64,7 +65,6 @@ public class BowlingGameTest {
 
     @Test
     void should_return_sum_when_calculate_given_Round_10_is_strike() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 1, 2, 10, 5, 5, 1, 2, 1, 2, 5, 5, 1, 2, 1, 2, 10, 10, 10};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(100, result);
@@ -72,7 +72,6 @@ public class BowlingGameTest {
 
     @Test
     void should_return_sum_when_calculate_given_Round_10_is_spare() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 1, 2, 10, 5, 5, 1, 2, 1, 2, 5, 5, 1, 2, 1, 2, 5, 5, 10};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(90, result);
@@ -80,7 +79,6 @@ public class BowlingGameTest {
 
     @Test
     void should_return_sum_when_calculate_given_all_strike() {
-        BowlingGame bowlingGame = new BowlingGame();
         int[] numberOfKnockdowns = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int result = bowlingGame.calculate(numberOfKnockdowns);
         assertEquals(300, result);
